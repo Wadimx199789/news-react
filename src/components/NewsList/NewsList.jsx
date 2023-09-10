2b28b845dc910f7b31a816347464ed0f54dee8c9
Wadimx199789 <martinkewichwadim@gmail.com>
2023-09-10 11:11:44 +0300

    feat: added new app

---
+++ src/components/NewsList/NewsList.jsx
@@ -0,0 +1,16 @@
+import NewsItem from "../NewsItem/NewsIte";
+import styles from "./styles.module.css";
+
+const NewsList = ({ news }) => {
+  return (
+    <ul className={styles.list}>
+      {news.map((item) => {
+        return <NewsItem key={item.id} item={item} />;
+      })}
+    </ul>
+  );
+};
+
+
+
+export default NewsList;
